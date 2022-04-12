97b31740f6c4dae037eb6ed73cd0e0711223fa50
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-04-12 09:24:33 -0300

    Add food class

---
+++ days_20_21/food.py
@@ -0,0 +1,21 @@
+from random import randint
+
+from turtle import Turtle
+
+SCREEN_LIMITS = 600 / 2 - 20
+
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+
+        self.shape("circle")
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
+        self.color("blue")
+        self.speed("fastest")
+
+        self.penup()
+
+        random_x = randint(-SCREEN_LIMITS, SCREEN_LIMITS)
+        random_y = randint(-SCREEN_LIMITS, SCREEN_LIMITS)
+        self.goto(x=random_x, y=random_y)
