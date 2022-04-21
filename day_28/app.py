d141fbae14147f9aa629c102f425b573ad244b5c
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-04-21 20:52:23 -0300

    Add support for pause, continue and skip

--- day_28/app.py
+++ day_28/app.py
@@ -14,6 +14,7 @@ class App(Pomodoro):
         super().__init__()
 
         self.job = None
+        self.after_id = None
 
         self.window = tkinter.Tk()
         self.window.title("Pomodoro Timer")
@@ -33,7 +34,7 @@ class App(Pomodoro):
                                                   font=(FONT_NAME, 26, "bold"))
         self.canvas.grid(column=1, row=1)
 
-        self.button_main = tkinter.Button(text="Start", command=self.process_job)
+        self.button_main = tkinter.Button(text="Start", width=8, command=self.process_job)
         self.button_main.grid(column=0, row=2)
 
         tkinter.Button(text="Reset").grid(column=2, row=2)
@@ -45,25 +46,44 @@ class App(Pomodoro):
         if self.job is None:
             self.job = "work"
             self.header.config(text="Work", fg=GREEN)
+            self.button_main.config(text="Pause", width=8, command=self.pause_job)
             self.start_timer(self.WORK_MIN)
+
         elif self.job == "work":
             self.job = "short-break"
+            self.button_main.config(text="Skip", width=8, command=self.process_job)
             self.header.config(text="Break", fg=PINK)
             self.start_timer(self.SHORT_BREAK_MIN)
+
         elif self.job == "short-break":
-            self.header.config(text="Timer", fg=GREEN)
             self.job = None
+            self.header.config(text="Timer", fg=GREEN)
+            self.button_main.config(text="Start", width=8, command=self.process_job)
+            if self.after_id is not None:
+                self.window.after_cancel(self.after_id)
+                self.time_left = 0
+                self.refresh_timer()
 
     def start_timer(self, time):
         self.time_left = 60 * time
+        self.count_down()
+
+    def pause_job(self):
+        self.window.after_cancel(self.after_id)
+        self.after_id = None
+        self.button_main.config(text="Continue", width=8, command=self.continue_job)
+
+    def continue_job(self):
+        self.button_main.config(text="Pause", width=8, command=self.pause_job)
         self.count_down()
 
     def count_down(self):
         self.refresh_timer()
         if self.time_left > 0:
             self.time_left -= 1
-            self.window.after(1000, self.count_down)
+            self.after_id = self.window.after(1000, self.count_down)
         else:
+            self.after_id = None
             self.process_job()
 
     def refresh_timer(self):
