3c3ceff7846cebb7a8619aa833b1409c6b054b41
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-04-21 17:05:24 -0300

    Add basic count down

--- day_28/app.py
+++ day_28/app.py
@@ -13,6 +13,8 @@ class App(Pomodoro):
     def __init__(self):
         super().__init__()
 
+        self.has_lock = False
+
         self.window = tkinter.Tk()
         self.window.title("Pomodoro Timer")
         self.window.config(padx=100, pady=50, bg=YELLOW)
@@ -23,15 +25,37 @@ class App(Pomodoro):
                       font=(FONT_NAME, 26, "bold")
                       ).grid(column=1, row=0)
 
-        canvas = tkinter.Canvas(width=200, height=224,
-                                bg=YELLOW, highlightthickness=0)
-        canvas.create_image(100, 112, image=self.tomato_img)
-        canvas.create_text(100, 130, text="00:00",
-                           fill="white", font=(FONT_NAME, 26, "bold"))
-        canvas.grid(column=1, row=1)
+        self.canvas = tkinter.Canvas(width=200, height=224,
+                                     bg=YELLOW, highlightthickness=0)
+        self.canvas.create_image(100, 112, image=self.tomato_img)
+        self.text_timer = self.canvas.create_text(100, 130, text="00:00",
+                                                  fill="white",
+                                                  font=(FONT_NAME, 26, "bold"))
+        self.canvas.grid(column=1, row=1)
 
-        tkinter.Button(text="Start").grid(column=0, row=2)
+        tkinter.Button(text="Start", command=self.start_timer).grid(column=0, row=2)
         tkinter.Button(text="Reset").grid(column=2, row=2)
 
     def launch(self):
         self.window.mainloop()
+
+    def start_timer(self):
+        if self.has_lock:
+            return
+
+        self.has_lock = True
+        self.time_left = 60 * self.WORK_MIN
+        self.count_down()
+
+    def count_down(self):
+        self.refresh_timer()
+        if self.time_left > 0:
+            self.time_left -= 1
+            self.window.after(1000, self.count_down)
+
+    def refresh_timer(self):
+        sec = self.time_left % 60
+        min = int((self.time_left - sec) / 60)
+
+        self.canvas.itemconfig(self.text_timer,
+                               text=f"{min:02d}:{sec:02d}")
--- day_28/pomodoro.py
+++ day_28/pomodoro.py
@@ -5,4 +5,8 @@ LONG_BREAK_MIN = 20
 
 class Pomodoro:
     def __init__(self):
-        ""
+        self.WORK_MIN = WORK_MIN
+        self.SHORT_BREAK_MIN = SHORT_BREAK_MIN
+        self.LONG_BREAK_MIN = LONG_BREAK_MIN
+
+        self.time_left = 0
