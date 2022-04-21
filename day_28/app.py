683f63d1010d1f8e373b4edb9e0cdab17eaeb27a
Guilherme Tadashi Maeoka <guim.space@gmail.com>
2022-04-21 20:37:07 -0300

    Assign jobs for process control

--- day_28/app.py
+++ day_28/app.py
@@ -13,7 +13,7 @@ class App(Pomodoro):
     def __init__(self):
         super().__init__()
 
-        self.has_lock = False
+        self.job = None
 
         self.window = tkinter.Tk()
         self.window.title("Pomodoro Timer")
@@ -21,9 +21,9 @@ class App(Pomodoro):
 
         self.tomato_img = tkinter.PhotoImage(file="tomato.png")
 
-        tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW,
-                      font=(FONT_NAME, 26, "bold")
-                      ).grid(column=1, row=0)
+        self.header = tkinter.Label(text="Timer", fg=GREEN, bg=YELLOW,
+                                    font=(FONT_NAME, 26, "bold"))
+        self.header.grid(column=1, row=0)
 
         self.canvas = tkinter.Canvas(width=200, height=224,
                                      bg=YELLOW, highlightthickness=0)
@@ -33,18 +33,29 @@ class App(Pomodoro):
                                                   font=(FONT_NAME, 26, "bold"))
         self.canvas.grid(column=1, row=1)
 
-        tkinter.Button(text="Start", command=self.start_timer).grid(column=0, row=2)
+        self.button_main = tkinter.Button(text="Start", command=self.process_job)
+        self.button_main.grid(column=0, row=2)
+
         tkinter.Button(text="Reset").grid(column=2, row=2)
 
     def launch(self):
         self.window.mainloop()
 
-    def start_timer(self):
-        if self.has_lock:
-            return
+    def process_job(self):
+        if self.job is None:
+            self.job = "work"
+            self.header.config(text="Work", fg=GREEN)
+            self.start_timer(self.WORK_MIN)
+        elif self.job == "work":
+            self.job = "short-break"
+            self.header.config(text="Break", fg=PINK)
+            self.start_timer(self.SHORT_BREAK_MIN)
+        elif self.job == "short-break":
+            self.header.config(text="Timer", fg=GREEN)
+            self.job = None
 
-        self.has_lock = True
-        self.time_left = 60 * self.WORK_MIN
+    def start_timer(self, time):
+        self.time_left = 60 * time
         self.count_down()
 
     def count_down(self):
@@ -52,6 +63,8 @@ class App(Pomodoro):
         if self.time_left > 0:
             self.time_left -= 1
             self.window.after(1000, self.count_down)
+        else:
+            self.process_job()
 
     def refresh_timer(self):
         sec = self.time_left % 60
